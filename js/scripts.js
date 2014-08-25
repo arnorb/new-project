41ba19c753189e0586513dc6c1b31906f9d670f5
Arnór Bogason <arnor@arnor.is>
2014-08-25 14:48:10 +0000

    Added base files

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// var headerResize = $('.site-header').height(0.75*$(window).height());
+
+// $(document).ready(function() {
+//   headerResize();
+// });
+
+// $(window).resize(function() {
+//   headerResize();
+// });
