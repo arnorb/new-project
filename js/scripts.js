df7f696c1f58de8d58f4d6fee8c44b8e90113ed6
Arnór Bogason <arnor@arnor.is>
2014-08-25 14:58:24 +0000

    remove old junk

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-// var headerResize = $('.site-header').height(0.75*$(window).height());
-
-// $(document).ready(function() {
-//   headerResize();
-// });
-
-// $(window).resize(function() {
-//   headerResize();
-// });
